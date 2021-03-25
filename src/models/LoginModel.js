7968ce63e4ddbc68c1628e456a0d878e24bfd8da
Rodrigo Nogueira <you@example.com>
2021-03-25 08:25:28 -0300

    bcrypt e verificando se usuario já existe

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs')
 
 const LoginSchema = new mongoose.Schema({
   email: { type: String, required: true },
@@ -18,6 +19,14 @@ class Login {
   async register() {
     this.valida();
     if(this.errors.length > 0) return;
+    
+    await this.userExists();
+
+    if(this.errors.length > 0) return;
+
+    const salt = bcryptjs.genSaltSync();
+    this.body.password = bcryptjs.hashSync(this.body.password, salt);
+    
     try {
       this.user = await LoginModel.create(this.body)
     } catch(e) {
@@ -25,6 +34,11 @@ class Login {
     }
     // const user = await LoginModel.create(this.body)
     // return user;
+  }
+
+  async userExists() {
+    const user = await LoginModel.findOne({ email: this.body.email })
+    if(user) this.errors.push('Usúario já existe.')
   }
   
   valida() {
